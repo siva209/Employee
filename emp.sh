b78c4764ce558794cf91f9ee279acf90349df9ed
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-11 13:42:39 +0530

    [employee daily wages added by siva]

--- emp.sh
+++ emp.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
-randomCheck=$((RANDOM%2))
-
-if [ $randomCheck -eq 1 ]
+#!/bin/bash -x
+randomcheck=$(( RANDOM%3 ))
+if [ $randomcheck -eq 1 ]
 then
-  echo "Employee is present"
+        ratehour=10
+        hour=4
+        salary=$(($ratehour*$hour))
+elif [ $randomcheck -eq 2 ]
+then
+        ratehour=20
+        hour=8
+        salary=$(($ratehour*$hour))
 else
-echo "Empoyee is absent"
+        salary=0
 fi
+echo "the salary is:" $salary
 
 
 
