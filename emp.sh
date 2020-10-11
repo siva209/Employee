2a69b9acedcb047f5e29a5cbdaeb22757eb62759
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-11 14:33:01 +0530

    [ employee wages for month]

--- emp.sh
+++ emp.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -x
-check=$(( RANDOM%3 ))
-case $check in
-	1))
-        ratehour=10
-        hour=4
-        salary=$(($ratehour*$hour));;
+isfulltime=1
+isparttime=2
+numOfWorkingDays=20
+totalSalary=0
+rate=20
+for ((day=1;day<=$numOfWorkingDays;day++))
+do
+        random=$((RANDOM%3))
+        echo $random
+        case $random in
+                $isfulltime) hrs=8;;
+                $isparttime) hrs=4;;
+                 *) hrs=0;;
+        esac
+salary=$(($rate*$hrs))
+totalSalary=$(($salary+$totalSalary))
+echo "total salary is:" $totalSalary
+done
 
-	2))
-        ratehour=20
-        hour=8
-        salary=$(($ratehour*$hour));;
-	*)
-        salary=0;;
-esac
-echo "the salary is:" $salary
